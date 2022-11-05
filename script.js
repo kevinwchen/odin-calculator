945050be967fe6fbb89dd31e9e45adc29b334afd
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-05 19:33:07 +1100

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = function(operator,a,b) {
+  switch (operator) {
+    case "+":
+      return add(a,b);
+    case "-":
+      return subtract(a,b);
+    case "*":
+      return multiply(a,b);
+    case "/":
+      return divide(a,b);
+    default:
+      console.log("Invalid operator")
+  }
+}
