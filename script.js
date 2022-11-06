6ee72b6cc294cec27c12cac8d9d201cd3e2c0301
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-06 17:16:50 +1100

    Add base functions for numbers and operators

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const resetDisplay = () => {
+  displayScreen.textContent = "0";
+};
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -16,6 +20,30 @@ const operate = function(operator,a,b) {
     default:
       console.log("Invalid operator")
   }
-}
+};
 
-const display = document.querySelector('.display');
+const displayScreen = document.querySelector('.display');
+const numBtn = document.querySelectorAll('.numBtn');
+const opBtn = document.querySelectorAll('.opBtn');
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+
+// Set default display
+resetDisplay();
+
+// Clear button clears display string
+clearBtn.addEventListener('click', () => {
+  displayScreen.textContent = "";
+});
+
+// Delete button removes last character from display string
+deleteBtn.addEventListener('click', () => {
+  let displayContents = displayScreen.textContent;
+  displayScreen.textContent = displayContents.slice(0,-1);
+});
+
+numBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
