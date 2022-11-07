ae2212a781a38d21c68f6902a9a42e4791077bda
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-08 10:02:36 +1100

    Fix additional endpoints for operations

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ const numBtn = document.querySelectorAll('.numBtn');
 const opBtn = document.querySelectorAll('.opBtn');
 const eqBtn = document.querySelector('#equals');
 let selectedOperator = undefined;
-
-const resetDisplay = () => {
-  // Reset display screen
-  display.textContent = "0";
-}
+let lastClick = undefined;
 
 const setDisplay = (value) => {
   // Set display screen value
   display.textContent = value;
 }
 
+const resetDisplay = () => {
+  // Reset display screen
+  setDisplay(0);
+}
+
 const pushDisplay = (value) => {
   // Add value to display if screen is not full
   if (display.textContent.length < 13) {
-    display.textContent = display.textContent.concat(value);
-  };
+    setDisplay(display.textContent.concat(value));
+  }
 }
 
 const popDisplay = () => {
@@ -28,16 +29,17 @@ const popDisplay = () => {
   if (display.textContent.length === 1) {
     resetDisplay();
   } else {
-    display.textContent = display.textContent.slice(0,-1);
+    setDisplay(display.textContent.slice(0, -1));
   }
 }
 
-const resetNumbers = () => {
-  num1 = num2 = undefined;
+const resetMemory = () => {
+  num1 = num2 = selectedOperator = undefined;
 }
 
 const displayError = () => {
-  display.textContent = "error";
+  resetMemory();
+  setDisplay("Overflow");
 }
 
 const add = (a, b) => a + b;
@@ -45,7 +47,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = function(operator,a,b) {
+const operate = function(operator, a, b) {
   // Operate on two input numbers based on an operator
   switch (operator) {
     case "add":
@@ -57,16 +59,18 @@ const operate = function(operator,a,b) {
     case "divide":
       return divide(a, b);
     default:
-      return "error";
+      return "op error";
   }
 }
 
 const performCalc = () => {
-  let calculated = (operate(selectedOperator, num1, num2)).toString();
+  let calculated = operate(selectedOperator, num1, num2);
+  let calculatedString = calculated.toString();
   // Check if calculated value fits on display
-  if (calculated.length < 13) {
-    display.textContent = calculated;
-    selectedOperator = undefined;
+  if (calculatedString.length < 13) {
+    setDisplay(calculatedString); // Typical calculated value
+  } else if (Math.round(calculated) <= 9999999999999 && calculated >= 0.000000000001) {
+    setDisplay(calculatedString.slice(0, 13)); // Calculated value with decimal points
   } else {
     displayError();
   }
@@ -75,18 +79,20 @@ const performCalc = () => {
 // Clear button clears display string
 clearBtn.addEventListener('click', () => {
   resetDisplay();
-  resetNumbers();
+  resetMemory();
+  lastClick = "clear";
 });
 
 // Delete button removes last character from display string
 deleteBtn.addEventListener('click', () => {
   popDisplay();
+  lastClick = "delete";
 });
 
 // Update display on number click
 numBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    if (selectedOperator || num1) {
+    if (lastClick === "operator" || lastClick === "equals") {
       resetDisplay();
     }
     
@@ -95,31 +101,41 @@ numBtn.forEach((button) => {
     } else {
       pushDisplay(button.id);
     }
+    lastClick = "number";
   });
 });
 
 // Operator button functionality
 opBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    selectedOperator = button.id;
-
-    if (num1) {
+    if (lastClick === "number" && num1) {
+      num2 = parseFloat(display.textContent);
       performCalc();
-      num1 = parseInt(display.textContent);
-    } else {
-      num1 = parseInt(display.textContent);
+      num1 = parseFloat(display.textContent);
+      num2 = undefined;
+    } else if (lastClick === "number") {
+      num1 = parseFloat(display.textContent);
     }
+    selectedOperator = button.id;
+    lastClick = "operator";
   });
 });
 
+// Equals button functionality
 eqBtn.addEventListener('click', () => {
-  // Set second number for operation
-  num2 = parseInt(display.textContent);
-  performCalc();
-  selectedOperator = undefined;
-  num1 = parseInt(display.textContent);
+  if (selectedOperator && num1) {
+    if (lastClick === "equals") {
+      performCalc();
+      num1 = parseFloat(display.textContent);
+    } else {
+      num2 = parseFloat(display.textContent);
+      performCalc();
+      num1 = parseFloat(display.textContent);
+    }
+  }
+  lastClick = "equals";
 });
 
 // Set numbers and display on startup
-resetNumbers();
+resetMemory();
 resetDisplay();
